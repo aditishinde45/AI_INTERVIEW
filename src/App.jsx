d408d82c6aa89381ff5ecc732b3c8ed803c9ca69
aditishinde45 <aditishinde530@gmail.com>
2026-07-31 21:54:36 +0530

    add progress

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom';
+import Auth from './pages/Auth';
+import Home from './pages/Home';
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/auth' element={<Auth/>}/>
+    </Routes>
+  )
+}
+
+export default App
